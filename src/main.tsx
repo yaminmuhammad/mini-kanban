25c36e7966113984a0d3fece0b4eede3a9e5b037
yaminmuhammad <muhammadyamin2801@gmail.com>
2024-04-07 08:42:21 +0700

    add layout, Boards & routes

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import React from 'react'
+import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App.tsx'
 import './index.css'
+import { BrowserRouter } from 'react-router-dom'
+import { StyleSheetManager } from 'styled-components'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Suspense fallback={<div>Loading...</div>}>
+    <BrowserRouter>
+      <StyleSheetManager>
+        <App />
+      </StyleSheetManager>
+    </BrowserRouter>
+  </Suspense>
 )

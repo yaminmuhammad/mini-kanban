d59afd6c6d1b2b415ef795fe36312e7cd4f8dbfe
yaminmuhammad <1910631170108@student.unsika.ac.id>
2024-04-10 15:38:36 +0700

    add board type

--- src/pages/Boards/index.tsx
+++ src/pages/Boards/index.tsx
@@ -1,7 +1,20 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+import { useState } from 'react'
+import { DragDropContext } from 'react-beautiful-dnd'
+import { Columns } from '../../types';
 
 const Boards = () => {
+  const [columns, setColumns] = useState<Columns>(Boards);
+
   return (
-    <div>Boards</div>
+    <>
+      <DragDropContext onDragEnd={(result: any)=>console.log(result)}>
+
+        <div className="w-full flex items-start justify-between px-5 md:gap-0 gap-10">
+
+        </div>
+      </DragDropContext>
+    </>
   )
 }
 
